77fbbc0914c38118cbde8c3ca72d375472d42314
kefitaib <taibkefi@yahoo.fr>
2020-02-05 12:43:24 +0000

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int int_var;
 long int long_int_var;
 long long int long_long_int_var;
 float float_var;
-printf("Size of a char: %c byte(s)\n", sizeof(char_var));
-printf("Size of int: %i byte(s)\n", sizeof(int_var));
-printf("Size of long int: %i byte(s)\n", sizeof(long_int_var));
-printf("Size of long long int: %i byte(s)\n", sizeof(long_long_int_var));
-printf("Size of float: %f byte(s)\n", sizeof(float_var));
+printf("Size of a char: %d byte(s)\n", sizeof(char_var));
+printf("Size of int: %d byte(s)\n", sizeof(int_var));
+printf("Size of long int: %d byte(s)\n", sizeof(long_int_var));
+printf("Size of long long int: %d byte(s)\n", sizeof(long_long_int_var));
+printf("Size of float: %d byte(s)\n", sizeof(float_var));
 return (0);
 }
