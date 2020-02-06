1bdb1e601fbb52f5eabb78c301313c4fd22d2546
kefitaib <taibkefi@yahoo.fr>
2020-02-06 12:57:35 +0000

    C

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - print the alpabet in lower case and uppercase
+ * @void: No parameters
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+char c;
+for (x = 48 ; x <= 58 ; c++)
+{
+putchar(c);
+}
+for (c = 'A' ; c <= 'F' ; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
