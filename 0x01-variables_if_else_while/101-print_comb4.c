3affb0ecb46b6a3ea45fde2a228b477d7b5235ac
kefitaib <taibkefi@yahoo.fr>
2020-02-06 21:13:46 +0000

    C

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers from 0 to 99 with double digit
+ * @void: No parameters
+ * Return: 0
+ */
+int main(void)
+{
+int i, j, k, x = 0, y = 0;
+for (i = 48  ; i < 56 ; i++)
+{
+for (j = 48 + x ; j < 57 ; j++)
+{
+for (k = 48 + y ; k < 58 ; k++)
+{
+if (((j != i) && (i != k) && (k != j)) || (i + j + k == 168))
+{
+putchar(i);
+putchar(j);
+putchar(k);
+}
+if (((i + j + k) < 168) && ((j != i) && (i != k) && (k != j)))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+x++;
+}
+putchar('\n');
+return (0);
+}
