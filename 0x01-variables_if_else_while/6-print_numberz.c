8bf88bd416077d2d9016fbc1ba4c73746651388d
kefitaib <taibkefi@yahoo.fr>
2020-02-06 12:30:57 +0000

    C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all single digit numbers of base 10 starting from 0
+ * @void: No parameters
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 48 ; i < 59 ; i++)
+{
+putchar(i);
+}
+putchar('\n');
+return (0);
+}
