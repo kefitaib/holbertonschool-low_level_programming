53cea38788f0aa60669208aa2d2ca3fef9da4869
kefitaib <taibkefi@yahoo.fr>
2020-02-06 12:45:24 +0000

    C

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,7 +16,7 @@ if (x > 5)
 printf("Last digit of %i is %i and is greater than 5\n", n, x);
 if (x == 0)
 printf("Last digit of %i is %i and is 0\n", n, x);
-if (x < 0)
+if ((x < 0) || ((x > 0) && (x < 6)))
 printf("Last digit of %i is %i and is less than 6 and not 0\n", n, x);
 return (0);
 }
