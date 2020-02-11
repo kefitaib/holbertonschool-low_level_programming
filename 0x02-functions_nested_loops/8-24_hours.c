628ad82cf9ee2bb04d90bf12526aad83df0fd0ab
kefitaib <taibkefi@yahoo.fr>
2020-02-11 00:18:38 -0800

    nested

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,6 @@
 #include "holberton.h"
 /**
- * jack_baue - prints every minute of the day of Jack Bauer, starting from 00:00 to 23:59.
+ * jack_bauer - prints every minute of the day of Jack Bauer.
  * @void: no parameters.
  *
  * Return: x.
@@ -11,7 +11,7 @@ int i, j, k, x, y, w = 9;
 for (i = 0 ; i <= 2 ; i++)
 {
 if (i == 2)
-w= 3;
+w = 3;
 for (j = 0 ; j <= w ; j++)
 {
 for (k = 0 ; k <= 59 ; k++)
