2a0d6a65ad4abdbaf2fc63999ed62cee7475e274
kefitaib <taibkefi@yahoo.fr>
2020-02-11 04:10:12 -0800

    bedugging

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int i;
 printf("Infinite loop incoming :(\n");
 
 i = 0;
-/* this loop is the cause of the infinite loop  */
+/*
 while (i < 10)
 {
 putchar(i);
 }
-
+*/
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
