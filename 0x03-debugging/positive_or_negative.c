fb7a7c07271c80bbf95c28d3ab196492b0995f91
kefitaib <taibkefi@yahoo.fr>
2020-02-11 03:27:38 -0800

    debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * positive_or_negativ - test if a number is positive or negative
+ * @i: No parameters
+ * Return: 0
+ */
+int positive_or_negative(int i)
+{
+if (i == 0)
+printf("%i is zero\n", i);
+else if (i > 0)
+printf("%i is positive\n", i);
+else
+printf("%i is negative\n", i);
+return (0);
+}
