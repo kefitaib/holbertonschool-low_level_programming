5081fe8fc13d0725ce26fa236c1cb0c9389460f5
kefitaib <taibkefi@yahoo.fr>
2020-02-11 13:34:51 -0800

    nested

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the first 50 Fibonacci.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+double i, f1 = 0, f2 = 0, sum = 0;
+printf("%i, %i, ", 1, 2);
+f1 = 2;
+f2 = 1 + 2;
+printf("%.0f, ", f2);
+for (i = 4 ; i <= 98 ; i++)
+{
+sum = f1 + f2;
+if (i != 98)
+printf("%.0f, ", sum);
+else
+printf("%.0f\n", sum);
+f1 = f2;
+f2 = sum;
+}
+return (0);
+}
