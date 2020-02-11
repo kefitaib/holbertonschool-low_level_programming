c1b585a62755fb3878568f5f0223282bb699fc89
kefitaib <taibkefi@yahoo.fr>
2020-02-11 13:47:42 -0800

    nested

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,7 +7,8 @@
  */
 int main(void)
 {
-double i, f1 = 0, f2 = 0, sum = 0;
+int i;
+double f1 = 0, f2 = 0, sum = 0;
 printf("%i, %i, ", 1, 2);
 f1 = 1;
 f2 = 2;
@@ -15,7 +16,7 @@ for (i = 3 ; i <= 98 ; i++)
 {
 sum = f1 + f2;
 if (i != 98)
-printf("%.0f, ", sum);
+printf("%.0f, \n", sum);
 else
 printf("%.0f\n", sum);
 f1 = f2;
