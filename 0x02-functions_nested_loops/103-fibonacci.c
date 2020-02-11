65fb49f8c92f3a1c84d5284dab8d72bfe843c0f1
kefitaib <taibkefi@yahoo.fr>
2020-02-11 12:57:36 -0800

    nested

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of the even-valued terms of Fibonacci sequence.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+long int f1 = 0, f2 = 0, sum = 0, sum_even;
+f1 = 2;
+f2 = 3;
+sum_even = f1;
+while(sum <= 4000000)
+{
+sum = f1 + f2;
+if (sum % 2 == 0)
+sum_even += sum;
+f1 = f2;
+f2 = sum;
+}
+printf("%li\n", sum_even);
+return (0);
+}
