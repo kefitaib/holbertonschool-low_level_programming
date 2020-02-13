368f335be770bfc7b8e58fb9d8ab7f3417a7b765
kefitaib <taibkefi@yahoo.fr>
2020-02-13 02:39:28 -0800

    draws a straight line in the terminal.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * print_line - draws a straight line in the terminal.
+ * @n: No parameters
+ * Return: void
+ */
+
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0 ; i < n ; i++)
+_putchar('_'); 
+_putchar('\n');
+}
+else
+_putchar('\n');
+}
