ce8f77ecc1f11bc483e2600a56de09adbcb3be40
kefitaib <taibkefi@yahoo.fr>
2020-02-13 10:11:13 -0800

    prints a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+/**
+ * print_triangle - draws a diagonal line on the terminal.
+ * @size: No parameters
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+int i, j, k, x = 1;
+if (size > 0)
+{
+for (i = 0 ; i < size ; i++)
+{
+for (j = 0 ; j < (size - x) ; j++)
+_putchar(' ');
+for (k = (size - x) ; k < size ; k++) 
+_putchar('#');
+_putchar('\n');
+x++;
+}
+}
+else
+_putchar('\n');
+}
