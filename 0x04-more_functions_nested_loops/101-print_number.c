1e5691550fef6da7fb08ec1db17e71548d8251a7
kefitaib <taibkefi@yahoo.fr>
2020-02-13 14:04:38 -0800

    holberton

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,9 +7,16 @@
 
 void print_number(int n)
 {
-int x, j, i = 0, n1, n2 = n, k = 1;
+int x, j, i = 0, n1, k = 1;
+if (n == 0)
+_putchar(48);
+else 
+{
 if (n < 0)
+{
+_putchar('-');
 n = -n;
+}
 n1 = n;
 while (n > 0)
 {
@@ -18,12 +25,6 @@ i++;
 k *= 10;
 }
 k /= 10;
-if (n2 == 0)
-_putchar(48);
-else
-{
-if (n2 < 0)
-_putchar('-');
 for (j = 0 ; j < i ; j++)
 {
 x = n1 / k;
