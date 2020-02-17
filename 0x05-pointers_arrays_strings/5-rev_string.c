a32e4f6d3e2d91ae06d9c8447cf9f0d9918859a2
kefitaib <taibkefi@yahoo.fr>
2020-02-17 05:19:49 -0800

    reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * rev_string - prints a string, in reverse.
+ * @*s : string.
+ * Return: void.
+ */
+
+void rev_string(char *s)
+{
+char s1[509];
+int i = 0, j, k = 0, l = 0;
+while (s[i] != '\0')
+i++;
+
+for (j = i -1 ; j >= 0 ; j--)
+{
+s1[k] = s[j];
+k++;
+}
+for (l = 0 ; l < i ; l++)
+s[l] = s1[l];
+}
