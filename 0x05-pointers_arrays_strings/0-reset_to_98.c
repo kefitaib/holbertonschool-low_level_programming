07099c98dfd8089e6acc79d161bfc669e44d2f5a
kefitaib <taibkefi@yahoo.fr>
2020-02-17 02:01:25 -0800

    takes a pointer to an int as parameter and updates the value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * reset_to_98 - akes a pointer to an int as parameter and updates the value.
+ * @*n : integer
+ * Return: void.
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,6 +3,7 @@
 #define HOLBERTON_H
 
 int _putchar(char c);
+void reset_to_98(int *n);
 
 
 #endif
