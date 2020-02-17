1cadfbb4223c157792d035e27ba99b80988e76bd
kefitaib <taibkefi@yahoo.fr>
2020-02-17 06:10:35 -0800

    prints n elements of an array of integers

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_array - prints n elements of an array of integers.
+ * @*a : array.
+ * @n : array length.
+ * Return: void.
+ */
+
+void print_array(int *a, int n)
+{
+int i = 0;
+for (i = 0 ; i < n ; i++)  
+{  
+printf("%d", a[i]);
+if (i != n - 1)
+printf(", ");
+}
+printf("\n");
+}
