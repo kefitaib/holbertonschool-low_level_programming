37c16354b146b7dc147ef0d36391a1644aab7915
kefitaib <taibkefi@yahoo.fr>
2020-02-18 02:22:13 -0800

    prints n ele,ents of qn qrrqy of integers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 /**
  * print_array - prints n elements of an array of integers.
- * @*a : array.
+ * @a : array.
  * @n : array length.
  * Return: void.
  */
@@ -10,8 +10,8 @@
 void print_array(int *a, int n)
 {
 int i = 0;
-for (i = 0 ; i < n ; i++)  
-{  
+for (i = 0 ; i < n ; i++)
+{
 printf("%d", a[i]);
 if (i != n - 1)
 printf(", ");
