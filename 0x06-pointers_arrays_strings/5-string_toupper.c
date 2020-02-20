4f77c68cf98f0b14b961734db68a5d68a1a643a3
kefitaib <taibkefi@yahoo.fr>
2020-02-20 05:34:16 -0800

    changes all lowercase letters of a string to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * *string_toupper - changes all lowercase letters of 
+ a string to uppercase.
+ * @s : string.
+ * Return: Always 0.
+ */
+
+char *string_toupper(char *s)
+{
+int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] >= 'a' && s[i] <= 'z')
+s[i] -= 32;
+i++; 
+}
+return (s);
+}
