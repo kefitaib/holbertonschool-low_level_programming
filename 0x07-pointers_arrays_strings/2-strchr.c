0a654c84d8fec95789427c37d0709c4a45d871b5
kefitaib <taibkefi@yahoo.fr>
2020-02-24 10:12:18 -0800

    locates a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,16 @@
 #include "holberton.h"
 
 /**
- * *_strchr - encodes a string into 1337.
- * @s : destination string.
- * @c : source string.
- * Return: Always 0.
+ * *_strchr - locates a character in a string.
+ * @s : string.
+ * @c : caracter.
+ * Return: position of the caracter.
  */
 
 char *_strchr(char *s, char c)
 {
 char *st;
 int i = 0;
-if (s == 0)
-return (0);
 while (s[i] != '\0')
 {
 if (s[i] == c)
