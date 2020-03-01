d4e999f08be2f8ab5be32e678b9d579576ec14a1
kefitaib <taibkefi@yahoo.fr>
2020-03-01 03:36:40 -0800

    program that prints its name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,11 +3,11 @@
 /**
  * main - program that prints its name.
  * @argc : number of argements.
- * @argv : array containe the argements. 
+ * @argv : array containe the argements.
  * Return: 0.
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 (void)argc;
