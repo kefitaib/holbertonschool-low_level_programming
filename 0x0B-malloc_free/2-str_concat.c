410e0c24545215c872f4cb7827abcf8c66e73f36
kefitaib <taibkefi@yahoo.fr>
2020-03-03 06:33:38 -0800

    concatenates two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -28,10 +28,16 @@ int i = 0, j = 0, size1 = 0, size2 = 0, sizep = 0;
 char *p;
 
 if (s1 == NULL)
+{
+s1 = malloc(sizeof(char));
 s1 = '\0';
+}
 
 if (s2 == NULL)
+{
+s2 = malloc(sizeof(char));
 s2 = '\0';
+}
 
 size1 = _strlen(s1);
 size2 = _strlen(s2);
