b454ea526f19faf7d7cbaf7700f1355c83f15835
kefitaib <taibkefi@yahoo.fr>
2020-03-05 06:58:44 -0800

    concatenates two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,19 +1,17 @@
 #include "holberton.h"
 #include <stdlib.h>
 
-
 /**
- * _strlen - return the size of a string.
- * @s : string.
- * Return: pointer of char.
+ * *_strlen - returns a size of a string
+ * @s : pointer to a string.
+ * Return: int.
  */
 
 unsigned int _strlen(char *s)
 {
-unsigned int i = 0;
-while (s[i] != '\0')
-i++;
-return (i);
+if (*s == '\0')
+return (0);
+return (_strlen(++s) + 1);
 }
 
 
@@ -21,44 +19,49 @@ return (i);
  * *string_nconcat - concatenates two strings.
  * @s1 : first string.
  * @s2 : second string.
- * @n : size.
- * Return: pointer of char.
+ * @n : integer.
+ * Return: pointer.
  */
-
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, size1 = 0, size2 = 0, sizer = 0;
-char *sr;
+unsigned int i = 0, j = 0, size1 = 0, size2 = 0, sizep = 0;
+char *p;
 
 if (s1 == NULL)
 {
 s1 = malloc(sizeof(char));
-s1[0] = '\0';
+*s1 = '\0';
 }
 
 if (s2 == NULL)
 {
 s2 = malloc(sizeof(char));
-s2[0] = '\0';
+*s2 = '\0';
 }
 
 size1 = _strlen(s1);
 size2 = _strlen(s2);
 if (size2 > n)
 size2 = n;
-sizer = size1 + size2 + 1;
+sizep = size1 + size2 + 1;
 
-sr = malloc(sizeof(char) * sizer);
-if (sr == NULL)
+p = malloc(sizeof(char) * sizep);
+if (p == NULL)
 return (NULL);
 
-for (i = 0; s1[i] != '\0'; i++)
-sr[i] = s1[i];
+while (s1[i] != '\0')
+{
+p[i] = s1[i];
+i++;
+}
 
-for (j = 0; j < size2; j++, i++)
-sr[i] = s2[j];
-
-sr[i] = '\0';
-return (sr);
+while (j < size2)
+{
+p[i] = s2[j];
+j++;
+i++;
+}
+p[i] = '\0';
+return (p);
 }
