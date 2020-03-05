728d2008222c2489093520a973f18122df5868ab
kefitaib <taibkefi@yahoo.fr>
2020-03-05 09:15:45 -0800

    function that

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,13 +13,15 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
 char *tab;
+
 if (nmemb == 0 || size == 0)
 return (NULL);
+
 tab = malloc(nmemb * size);
 if (tab == NULL)
 return (NULL);
 
-for (i = 0; i < nmemb ; i++)
+for (i = 0; i < nmemb * size; i++)
 tab[i] = 0;
 
 return (tab);
