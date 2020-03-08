e61ee211d3c5672c8441df613b375b2c861a33f5
kefitaib <taibkefi@yahoo.fr>
2020-03-08 12:04:16 -0700

    check if a string is is a palindrome

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -8,30 +8,28 @@
 
 int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-return (0);
-return (_strlen_recursion(s + 1) + 1);
+	if (*s == '\0')
+		return (0);
+	return (_strlen_recursion(s + 1) + 1);
 }
 
 /**
  * palin - test if a string is a palindrome.
  * @p : string.
  * @x : integer.
+ * @i : integer.
  * Return: integer.
  */
 
-int palin(char *p, int x)
+int palin(char *p, int x, int i)
 {
-if (x <= 0)
-return (0);
-else
-{
-if (*p == p[x])
-return (palin(++p, --x));
-return (1);
+	if (i > x / 2)
+		return (1);
+	else if (p[i] != p[x - i])
+		return (0);
+	else
+		return (palin(p, x, ++i));
 }
-}
-
 
 /**
  * is_palindrome - test if a string is a palindrome.
@@ -41,8 +39,8 @@ return (1);
 
 int is_palindrome(char *s)
 {
-char *p = s;
-int x = -1;
-x += _strlen_recursion(s);
-return (palin(p, x));
+	char *p = s;
+	int x = 0, i = 0;
+	x = _strlen_recursion(s);
+	return (palin(p, x - 1, i));
 }
