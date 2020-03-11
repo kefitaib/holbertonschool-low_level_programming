b29b0fbebcd23e4321c6f90f283c81c44be2e588
kefitaib <taibkefi@yahoo.fr>
2020-03-11 13:07:41 -0700

    program that performs simple operations

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -12,6 +12,8 @@
 
 int main(int argc, char **argv)
 {
+	int (*f)(int, int);
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -31,7 +33,8 @@ int main(int argc, char **argv)
 		exit(100);
 	}
 
-	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	f = get_op_func(argv[2]);
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
 
 	return (0);
 }
