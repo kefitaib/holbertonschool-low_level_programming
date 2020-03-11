7ccab5d84b7fc13d4677a02946f45d57c2c7c561
kefitaib <taibkefi@yahoo.fr>
2020-03-11 08:40:31 -0700

    prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - callback to a function that prints a name
+ * @name: name of the person
+ * @f : pointer to a function.
+ * Return: Nothing.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
