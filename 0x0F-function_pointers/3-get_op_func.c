dd2d1bb4c26b1f3246a43fd461d29ea5206247bd
kefitaib <taibkefi@yahoo.fr>
2020-03-11 13:14:54 -0700

    contain the function that selects the correct function to perform

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -25,9 +25,8 @@ int (*get_op_func(char *s))(int, int)
 	while (ops[i].op != NULL)
 	{
 		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
+			break;
 		i++;
 	}
-	printf("Error\n");
-	exit(99);
+	return (ops[i].f);
 }
