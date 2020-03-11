87e3dd889d67ce84a99e724136d0b6a3363a1009
kefitaib <taibkefi@yahoo.fr>
2020-03-11 12:26:27 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,40 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Struct op
+ * @argv : an array of elements.
+ * @argc : number of elements.
+ * Return: Void.
+ */
+
+int main(int argc, char **argv)
+{
+	int (*f)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if ((argv[2][0] != '+' || argv[2][0] != '-' || argv[2][0] != '*' ||
+	     argv[2][0] != '/' || argv[2][0] != '%') && argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	if ((argv[2][0] == '/' || argv[2][0] == '%') && atoi(argv[3]) == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	f = get_op_func(argv[2]);
+	printf("%d\n", f(atoi(argv[1]), atoi(argv[3])));
+
+	return (0);
+}
