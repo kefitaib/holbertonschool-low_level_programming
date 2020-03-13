f84c06b25eb70fd3cc3abbf349359256665d7ab3
kefitaib <taibkefi@yahoo.fr>
2020-03-13 03:53:00 -0700

    prints the argements, followed by a new line

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - prints numbers, followed by a new line.
+ * @separator : string to be printed between numbers.
+ * @n : number of parameters based.
+ * Return: void.
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	if (n <= 0)
+		return;
+
+	va_start(list, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(list, int));
+		if (separator && i != n - 1)
+			printf("%s", separator);
+	}
+
+	putchar('\n');
+	va_end(list);
+}
