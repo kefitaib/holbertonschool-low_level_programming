561bacbb8eff1d0e39d18bd171c0960d69a26558
kefitaib <taibkefi@yahoo.fr>
2020-03-14 05:17:43 -0700

    function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -81,7 +81,7 @@ void print_all(const char * const format, ...)
 
 	va_start(lst, format);
 
-	while (format[i] != '\0')
+	while (format[i] != '\0' && format != NULL)
 	{
 		j = 0;
 		while (f[j] != '\0')
