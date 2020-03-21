b11cb4ad6ea8cc712f9e0ff8c0959103cb086f91
kefitaib <taibkefi@yahoo.fr>
2020-03-21 06:23:09 -0700

    print a message before the main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-void startupfun(void) __attribute__ ((constructor));
+void before_main(void) __attribute__ ((constructor));
 
 /**
- * startupfun - print a message before the main function is executed.
+ * before_main - print a message before the main function is executed.
  *
  * Return: voidAlways 0.
  */
 
-void startupfun(void)
+void before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
