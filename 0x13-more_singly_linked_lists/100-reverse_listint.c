7548195df76a9f2b6e4d5a44f3c8cf5d59217d0c
kefitaib <taibkefi@yahoo.fr>
2020-03-24 05:25:05 -0700

    Reverse a list

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -10,20 +10,22 @@ listint_t *reverse_listint(listint_t **head)
 {
 	listint_t *l = *head, *tmp;
 
-	*head = l->next;
-	l->next = NULL;
-	tmp = l;
-	l = *head;
-
-	while (l->next)
+	if (l)
 	{
 		*head = l->next;
-		l->next = tmp;
+		l->next = NULL;
 		tmp = l;
 		l = *head;
-	}
-	l->next = tmp;
-	*head = l;
 
+		while (l->next)
+		{
+			*head = l->next;
+			l->next = tmp;
+			tmp = l;
+			l = *head;
+		}
+		l->next = tmp;
+		*head = l;
+	}
 	return (*head);
 }
