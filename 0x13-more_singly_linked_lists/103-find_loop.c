0aef445142f70dfa2e7cc9c28819151b83179e52
kefitaib <taibkefi@yahoo.fr>
2020-03-24 13:20:51 -0700

    Find a loop inside a list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - find a loop in a list.
+ * @head : first element of the list.
+ * Return: pointer to the node where the loop started.
+ */
+
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *l = head;
+
+	while (l)
+	{
+		if (l <= l->next)
+			return (l->next);
+
+		l = l->next;
+	}
+	return (NULL);
+}
