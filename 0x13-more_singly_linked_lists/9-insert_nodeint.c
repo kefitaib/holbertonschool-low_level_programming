2afda42ee507799fb8aa2661246ab8086a5ad4b5
kefitaib <taibkefi@yahoo.fr>
2020-03-24 05:17:10 -0700

    inserts a new node at a given position

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,38 +16,41 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *l = *head, *tmp;
 	unsigned int i = 0, j;
 
-	while (l->next)
-	{
-		l = l->next;
-		i++;
-	}
-
-	if (idx > i + 1)
-		return (NULL);
-
 	tmp = malloc(sizeof(listint_t));
 	if (!tmp)
 		return (NULL);
-
 	tmp->n = n;
 	tmp->next = NULL;
-
-	if (idx == i + 1)
-		l->next = tmp;
-
-	else if (idx == 0)
+	if (idx == 0)
 	{
-		tmp->next = *head;
+		if (l)
+			tmp->next = *head;
 		*head = tmp;
+		return (tmp);
 	}
-	else
+	if (l)
 	{
-		l = *head;
-		for (j = 0; j < idx - 1; j++)
+		while (l->next)
+		{
 			l = l->next;
+			i++;
+		}
+		if (idx > i + 1)
+			return (NULL);
 
-		tmp->next = l->next;
-		l->next = tmp;
+		if (idx == i + 1)
+			l->next = tmp;
+		else
+		{
+			l = *head;
+			for (j = 0; j < idx - 1; j++)
+				l = l->next;
+
+			tmp->next = l->next;
+			l->next = tmp;
+		}
+		return (tmp);
 	}
-	return (tmp);
+	free(tmp);
+	return (NULL);
 }
