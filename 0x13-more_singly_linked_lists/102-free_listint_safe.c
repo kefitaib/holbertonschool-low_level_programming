69daca55ea64fbcb60cb4b557bfbeaabdc906e54
kefitaib <taibkefi@yahoo.fr>
2020-03-24 09:51:47 -0700

    frees a listint_t list

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint_safe - Free a list.
+ * @h : first element of the list.
+ * Return: Voidnumber of element in the list.
+ */
+
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *l;
+	size_t i = 0;
+
+	if (h)
+	{
+		l = *h;
+		while (l)
+		{
+			*h = l->next;
+			if (l <= *h)
+			{
+				free(l);
+				*h = NULL;
+				break;
+			}
+			free(l);
+			l = *h;
+			i++;
+		}
+	}
+	return (i);
+}
