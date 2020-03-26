e58d1a836753e0e3479b31cb62d51fba6f1d03b7
kefitaib <taibkefi@yahoo.fr>
2020-03-26 12:54:10 -0700

    sets the value of a bit to 1 at a given index

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,30 +2,6 @@
 #include <stdio.h>
 
 /**
- * _pow - calculate the power of an integer.
- * @x : the integer .
- * @y : nubber of times.
- * Return: result int.
- */
-
-int _pow(int x, int y)
-{
-	int i, res = 1;
-
-	if (y == 0)
-		return (1);
-
-	if (y == 1)
-		return (x);
-
-	for (i = 0; i < y; i++)
-		res *= x;
-
-	return (res);
-}
-
-
-/**
  * set_bit - sets the value of a bit to 1 at a given index.
  * @n : number.
  * @index : the index of the bit you want to get.
@@ -54,7 +30,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	t[index] = 1;
 
 	for (j = 0; j < i; j++)
-		x += t[j] * _pow(2, j);
+		x += t[j] << j;
 
 	*n = x;
 	return (1);
