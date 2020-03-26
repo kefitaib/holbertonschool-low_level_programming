74b2bd3285c719847a4cc8979a49f588f24e3749
kefitaib <taibkefi@yahoo.fr>
2020-03-26 11:47:20 -0700

    checks the endianness

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * get_endianness - checks the endianness.
+ * @void : void.
+ *
+ * Return: integer.
+ */
+
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+		return (1);
+
+	return (0);
+}
