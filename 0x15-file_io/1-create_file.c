5bf127565a311e472c9a729fc72d51bfaa3cf54a
kefitaib <taibkefi@yahoo.fr>
2020-03-31 08:08:05 -0700

    creates a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * create_file - check the code for Holberton School students.
+ * @filename : name of a file.
+ * @text_content : the content of the file.
+ * Return: integer.
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, nb, x;
+
+	if (!text_content)
+		return (-1);
+
+	if (!filename)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (1);
+
+	for (nb = 0; text_content[nb]; nb++)
+		;
+
+	x = write(fd, text_content, nb);
+	if (x == -1)
+		return (-1);
+
+	close(fd);
+	return (1);
+
+}
