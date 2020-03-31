cbcc63a8b5ac75b2e02ca1a280e11a2fa08ffe02
kefitaib <taibkefi@yahoo.fr>
2020-03-31 06:31:17 -0700

    ssize_t x = 0;

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - check the code for Holberton School students.
+ * @filename : name of a file.
+ * @letters : number of letters ot print.
+ * Return: t.
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t x;
+	int fd, nb;
+	char *s;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	s = malloc(letters);
+	nb = read(fd, s, letters);
+	if (nb == -1)
+		return (0);
+
+	x = write(STDOUT_FILENO, s, nb);
+	if (x == -1)
+		return (0);
+
+	free(s);
+	close(fd);
+	return (x);
+
+}
