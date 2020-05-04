053bd32e24d662eece5c9e4cd65d03873f1d6058
kefitaib <taibkefi@yahoo.fr>
2020-05-04 15:59:08 -0700

    Giga Millions

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!bin/bash
-wget -P /tmp https://github.com/kefitaib/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/z.so
-export LD_PRELOAD=/tmp/z.so
+wget -P /home https://github.com/kefitaib/holbertonschool-low_level_programming/blob/master/0x18-dynamic_libraries/z.so
+export LD_PRELOAD=/home/z.so
