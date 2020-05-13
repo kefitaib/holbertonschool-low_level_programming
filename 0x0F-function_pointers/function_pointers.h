855cb71f1eb65a99ec953b9ac11a7bb674cae32a
kefitaib <58983187+kefitaib@users.noreply.github.com>
2020-05-13 15:55:58 +0200

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
-
+#include <unistd.h>
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
