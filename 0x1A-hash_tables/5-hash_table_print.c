4a590985d53890e443059b4635965d342c6bdd64
kefitaib <taibkefi@yahoo.fr>
2020-05-29 05:23:07 -0700

    fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,7 +11,7 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t **arr = NULL, *tmp = NULL;
 	unsigned long int i = 0, x = 0;
 
-	if (!ht)
+	if (!ht || !ht->array)
 		printf("{}\n");
 
 	else
