9c46296b6d6ca3caeb6c395daa6df051190aa8ba
kefitaib <taibkefi@yahoo.fr>
2020-05-29 04:26:57 -0700

    fix code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -59,8 +59,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 	index = key_index((unsigned char *)key, ht->size);
-	if (check_key(ht->array[index], k, val) == 1)
-		return (1);
 
 	new = malloc(sizeof(hash_node_t));
 	if (!new)
