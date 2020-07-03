9ea14ad636bc07b989f138a1268d30d6029b349e
kefitaib <taibkefi@yahoo.fr>
2020-07-03 15:00:18 -0700

    island

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,24 +5,24 @@
 def island_perimeter(grid):
     """ grid """
 
-    i = 1
-    p = 0
+    h = len(grid) - 1
+    w = len(grid[0]) - 1
+    p, i = 0, 0
 
-    while i < len(grid) - 1:
-        j = 1
-
-        while j < len(grid[0]) - 1:
+    while i <= h:
+        j = 0
+        while j <= w:
             if grid[i][j] == 1:
-                if grid[i][j + 1] == 0:
+                if j == w or grid[i][j + 1] == 0:
                     p += 1
 
-                if grid[i][j - 1] == 0:
+                if grid[i][j - 1] == 0 or j == 0:
                     p += 1
 
-                if grid[i + 1][j] == 0:
+                if i == h or grid[i + 1][j] == 0:
                     p += 1
 
-                if grid[i - 1][j] == 0:
+                if grid[i - 1][j] == 0 or i == 0:
                     p += 1
 
             j += 1
