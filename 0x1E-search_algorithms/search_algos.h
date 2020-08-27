8abd2e60b929e2446ff05db178fbd3d684a187f8
kefitaib <taibkefi@yahoo.fr>
2020-08-27 06:13:58 -0700

    prototypes

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,14 @@
+#ifndef __HOLBERTON__
+#define __HOLBERTON__
+
+#include<unistd.h>
+#include <string.h>
+#include<stddef.h>
+#include <stdlib.h>
+
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
